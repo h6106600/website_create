a8c3fe0df8160a67dbcfe16a6d924fa066166285
luke <h6106600@gmail.com>
2023-08-02 16:05:30 +0800

    0802

--- view/home.php
+++ view/home.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
+  <div class="container-fluid" style="margin-left:10px;">
     <a class="navbar-brand" href="#">Logo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -33,7 +33,7 @@
           </ul>
         </li> -->
       </ul>
-	  <ul class="navbar-nav d-flex" style="margin-right:20px;">
+	  <ul class="navbar-nav d-flex" style="margin-right:10px;">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
