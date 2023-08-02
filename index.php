1ff46c429b6ff96d6105c3b78e90521d422bb76a
luke <h6106600@gmail.com>
2023-08-02 16:06:19 +0800

    0802

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-if($_SERVER['REQUEST_URI'] == '/'){
+if ($_SERVER['REQUEST_URI'] == '/') {
     require_once 'view/home.php';
 }
-
-
-?>
+if ($_SERVER['REQUEST_URI'] == '/1') {
+    require_once 'view/template1.php';
+}
